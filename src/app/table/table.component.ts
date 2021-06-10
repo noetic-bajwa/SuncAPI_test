0c08a42f1451d372f17f071950624a85630a21a4
Bajwa <anus@noeticworld.com>
2021-06-11 03:00:41 +0500

    Some tricky error

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,25 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , HostListener , ViewChild } from '@angular/core';
 import { DataService } from '../data.service';
 import { PageChangedEvent } from 'ngx-bootstrap/pagination';
 import { DatePipe } from '@angular/common';
+import { BsDatepickerDirective } from 'ngx-bootstrap/datepicker';
 
-
+interface IRange {
+  value: Date[];
+  label: string;
+}
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css']
 })
 export class TableComponent implements OnInit {
+  @ViewChild(BsDatepickerDirective, { static: false }) datepicker: BsDatepickerDirective;
+  
+   @HostListener('window:scroll')
+   onScrollEvent() {
+     this.datepicker.hide();
+     
+     
+   }
+
   currentPage:any;
   page:any=1;
   data:any={};
-  startDate:any;
-  EndDate:any;
+  StartingDate:any;
+  EndingDate:any;
+  maxDate=new Date();
+  sDate=new Date(new Date().setDate(new Date().getDate() - 7));
+  eDate=new Date();
+    
   
   
   
-  constructor(private dataService:DataService, private datePipe: DatePipe) { }
   
+  constructor(private dataService:DataService, private datePipe: DatePipe) {
+    
+   }
+   
 
   ngOnInit(): void {
     this.dataService.getData(this.page-1).subscribe(data=>{
@@ -59,7 +79,9 @@ export class TableComponent implements OnInit {
     
   }
 
-  onClickReset(){
+  onClickReset(StartDate:any,EndDate:any){
+    this.StartingDate='';
+    this.EndingDate='';
     this.dataService.getData('0').subscribe(data=>{
       this.data=data;
       this.currentPage = this.data['currentPage'];
@@ -72,6 +94,11 @@ export class TableComponent implements OnInit {
       
      })
     
+  }
+
+  onClickLast7Days(){
+    this.StartingDate=this.datePipe.transform(this.sDate,'MM/dd/yyyy');
+    this.EndingDate=this.datePipe.transform(this.eDate,'MM/dd/yyyy');
   }
 
 
